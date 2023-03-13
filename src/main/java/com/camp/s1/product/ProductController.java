f05b9795c57c065f4ec1475ca8c3c381caf89bd3
YoungDHi <sm930219@naver.com>
2023-03-13 13:58:40 +0900

    dd

---
+++ src/main/java/com/camp/s1/product/ProductController.java
@@ -0,0 +1,20 @@
+package com.camp.s1.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProductController {
+	
+	
+	
+	//List 출력
+	public ModelAndView getProductList() throws Exception {
+		
+		ModelAndView mv = new ModelAndView();
+		
+		return mv;
+		
+	}
+
+}
---
+++ src/main/java/com/camp/s1/product/ProductDTO.java
@@ -0,0 +1,38 @@
+package com.camp.s1.product;
+
+public class ProductDTO {
+	
+	private Double productNum;
+	private String name;
+	private String contents;
+	private String status;
+	
+	
+	public Double getProductNum() {
+		return productNum;
+	}
+	public void setProductNum(Double productNum) {
+		this.productNum = productNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+
+}

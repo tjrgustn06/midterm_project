b8d367ff48cc234909d0d36527c5e3b861b3161a
YoungDHi <sm930219@naver.com>
2023-04-13 16:37:15 +0900

    interceptorAndException

--- src/main/java/com/camp/s1/interceptors/OwnerCheckInterceptor.java
+++ src/main/java/com/camp/s1/interceptors/OwnerCheckInterceptor.java
@@ -1,8 +1,64 @@
 package com.camp.s1.interceptors;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.camp.s1.board.BbsDTO;
+import com.camp.s1.board.notice.NoticeDAO;
+import com.camp.s1.board.qna.QnaDAO;
+import com.camp.s1.camping.review.CampReviewDAO;
+import com.camp.s1.member.MemberDTO;
+import com.camp.s1.product.review.ProductReviewDAO;
 
 public class OwnerCheckInterceptor extends HandlerInterceptorAdapter {
 	
+	@Autowired
+	private NoticeDAO noticeDAO;
+	@Autowired
+	private QnaDAO qnaDAO;
+	@Autowired
+	private CampReviewDAO campReviewDAO;
+	@Autowired
+	private ProductReviewDAO productReviewDAO;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		Long num = Long.parseLong(request.getParameter("num"));
+		
+		String uri = request.getRequestURI();
+		uri = uri.substring(1,uri.lastIndexOf("/"));
+		BbsDTO bbsDTO = new BbsDTO();
+		bbsDTO.setNum(num);
+		if(uri.equals("notice")) {
+			bbsDTO = noticeDAO.getBoardDetail(bbsDTO);
+		} else if(uri.equals("qna")) {
+			bbsDTO = qnaDAO.getBoardDetail(bbsDTO);
+		} else if(uri.equals("camp")) {
+			bbsDTO = campReviewDAO.getBoardDetail(bbsDTO);
+		} else {
+			bbsDTO = productReviewDAO.getBoardDetail(bbsDTO);
+		}
+		
+		MemberDTO memberDTO = (MemberDTO)request.getSession().getAttribute("member");
+		if(!memberDTO.getId().equals(bbsDTO.getWriter())) {
+			request.setAttribute("result", "작성자만 가능");
+			request.setAttribute("url", "./list");
+			
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/common/result.jsp");
+			view.forward(request, response);
+			
+			return false;
+		}
+		
+		
+		
+		return true;
+	}
 
 }
--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -1,6 +1,22 @@
 $(function() {
     "use strict";
 
+    let nav_offset_top = $('.header_area').height()+50; 
+    
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+
     $(".preloader").fadeOut();
     // this is for close icon when navigation open in mobile view
     $(".nav-toggler").on('click', function() {
